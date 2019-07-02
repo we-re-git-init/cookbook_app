200cec3d3be5a65797c8debd43d3dd1097e306f8
Brian <riceb53@gmail.com>
2019-07-02 18:40:44 -0500

    add hours minutes prep_time

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,13 @@ class Recipe < ApplicationRecord
   def directions_list
     directions.split(", ")
   end
+
+  def hours_minutes_prep_time
+    result = ""
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result += "#{hours} hours " if hours > 0
+    result += "#{minutes} minutes" if minutes > 0
+    result
+  end
 end
