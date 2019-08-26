a544c4edaf1e47f65e919c5a23e7a5b921c5caed
Brian <riceb53@gmail.com>
2019-08-26 17:55:56 -0500

    remove fullstack routes

--- config/routes.rb
+++ config/routes.rb
@@ -25,11 +25,11 @@ Rails.application.routes.draw do
   end
 
   # fullstack
-  get '/recipes' => 'recipes#index'
-  get '/recipes/new' => 'recipes#new'
-  get '/recipes/:id' => 'recipes#show'
-  post '/recipes' => 'recipes#create'
-  get '/recipes/:id/edit' => 'recipes#edit'
-  patch '/recipes/:id' => 'recipes#update'
-  delete '/recipes/:id' => 'recipes#destroy'
+  # get '/recipes' => 'recipes#index'
+  # get '/recipes/new' => 'recipes#new'
+  # get '/recipes/:id' => 'recipes#show'
+  # post '/recipes' => 'recipes#create'
+  # get '/recipes/:id/edit' => 'recipes#edit'
+  # patch '/recipes/:id' => 'recipes#update'
+  # delete '/recipes/:id' => 'recipes#destroy'
 end
